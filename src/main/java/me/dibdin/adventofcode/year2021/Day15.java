18d88df84870a67192452cb6b773cc7afaf5b022
Pete Dibdin <pete@dibdin.me>
2021-12-16 20:36:48 +0000

    Reduce queue searching to improve speed x5

--- src/main/java/me/dibdin/adventofcode/year2021/Day15.java
+++ src/main/java/me/dibdin/adventofcode/year2021/Day15.java
@@ -1,6 +1,7 @@
 package me.dibdin.adventofcode.year2021;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
@@ -30,6 +31,7 @@ public class Day15 extends AbstractChallenge {
         public int x;
         public int y;
         public int weight;
+        public boolean visited;
 
         @Override
         public boolean equals(Object obj) {
@@ -48,6 +50,7 @@ public class Day15 extends AbstractChallenge {
     }
 
     class CompareNodeweight implements Comparator<Node> {
+
         public int compare(Node n1, Node n2) {
             return n1.weight - n2.weight;
         }
@@ -58,8 +61,10 @@ public class Day15 extends AbstractChallenge {
     }
 
     /**
-     * Find the shortest path from top let to bottom right, using the weightings in the grid.
+     * Find the shortest path from top let to bottom right, using the weightings in
+     * the grid.
      * Implementations use the Dijkstra's alogrithm with a priority queue.
+     * 
      * @param grid the input grod
      * @return the total weight of the shortest path
      */
@@ -74,6 +79,7 @@ public class Day15 extends AbstractChallenge {
                 nodes[y][x].x = x;
                 nodes[y][x].y = y;
                 nodes[y][x].weight = Integer.MAX_VALUE;
+                nodes[y][x].visited = false;
                 pq.add(nodes[y][x]);
             }
         }
@@ -83,16 +89,19 @@ public class Day15 extends AbstractChallenge {
         nodes[0][0].weight = 0;
         pq.add(nodes[0][0]);
 
+        Node endNode = nodes[nodes.length -1][nodes[0].length-1];
+
         while (pq.size() > 0) {
             // remove the node with the smallest weight from the queue
             Node current = pq.poll();
+            current.visited = true;
 
             // have we reached the end point
-            if ((current.x == grid[0].length - 1) && (current.y == grid.length - 1)) {
+            if (current.equals(endNode)) {
                 return current.weight;
             }
 
-            if ((pq.size() % 100) == 0) {
+            if ((pq.size() % 1000) == 0) {
                 int target = (grid.length * grid[0].length);
                 System.out.println((target - pq.size()) + " / " + target);
             }
@@ -100,41 +109,58 @@ public class Day15 extends AbstractChallenge {
             // look left
             if (current.x > 0) {
                 Node left = nodes[current.y][current.x - 1];
-                if (pq.remove(left)) {
-                    left.weight = Integer.min(left.weight, current.weight + grid[current.y][current.x - 1]);
-                    pq.add(left);
+                if (!left.visited) {
+                    int tempWeight = current.weight + grid[current.y][current.x - 1];
+                    if (tempWeight < left.weight) {
+                        pq.remove(left);
+                        left.weight = tempWeight;
+                        pq.add(left);
+                    }
                 }
             }
 
             // look right
-            if (current.x < grid[0].length - 1) {
+            if (current.x < (grid[0].length - 1)) {
                 Node right = nodes[current.y][current.x + 1];
-                if (pq.remove(right)) {
-                    right.weight = Integer.min(right.weight, current.weight + grid[current.y][current.x + 1]);
-                    pq.add(right);
+                if (!right.visited) {
+                    int tempWeight = current.weight + grid[current.y][current.x + 1];
+                    if (tempWeight < right.weight) {
+                        pq.remove(right);
+                        right.weight = tempWeight;
+                        pq.add(right);
+                    }
                 }
             }
 
             // look up
             if (current.y > 0) {
                 Node up = nodes[current.y - 1][current.x];
-                if (pq.remove(up)) {
-                    up.weight = Integer.min(up.weight, current.weight + grid[current.y - 1][current.x]);
-                    pq.add(up);
+                if (!up.visited) {
+                    int tempWeight = current.weight + grid[current.y - 1][current.x];
+                    if (tempWeight < up.weight) {
+                        pq.remove(up);
+                        up.weight = tempWeight;
+                        pq.add(up);
+                    }
                 }
             }
 
             // look down
-            if (current.y < grid.length - 1) {
+            if (current.y < (grid.length - 1)) {
                 Node down = nodes[current.y + 1][current.x];
-                if (pq.remove(down)) {
-                    down.weight = Integer.min(down.weight, current.weight + grid[current.y + 1][current.x]);
-                    pq.add(down);
+                if (!down.visited) {
+                    int tempWeight = current.weight + grid[current.y + 1][current.x];
+                    if (tempWeight < down.weight) {
+                        pq.remove(down);
+                        down.weight = tempWeight;
+                        pq.add(down);
+                    }
                 }
             }
         }
 
-        return nodes[grid.length - 1][grid[0].length - 1].weight;
+        // no path found
+        return -1;
     }
 
     /**
@@ -173,7 +199,6 @@ public class Day15 extends AbstractChallenge {
 
         return findShortestPath(enlargedPuzzle);
     }
-    
 
     /**
      * Take the stream of strings, and decode the input into puzzle data for this
